6565d22849a0adf870a0efbf3015f56237bb0449
Gabriela Bosetti <gabybosetti@gmail.com>
2021-04-09 22:13:11 -0300

    fix: Add bootstrap to the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';  
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -15,6 +14,7 @@ import { PersonasComponent } from './personas/personas.component';
 import { EmpleadosComponent } from './empleados/empleados.component';
 import { ObstetrasComponent } from './obstetras/obstetras.component';
 import { ClinicasComponent } from './clinicas/clinicas.component';
+import 'bootstrap';
 
 @NgModule({
   declarations: [
