98cf8d56a3cbad92915a379f3aa75dc86e7d5245
Emmanuel Arias <eamanu@yaerobi.com>
2021-04-20 11:31:01 -0300

    Fix login
    Import ReactiveFormsModule from @angular/forms, eso permite
    aparentemente que funcione el FormsModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 //fake backend
@@ -19,7 +20,6 @@ import { EmpleadosComponent } from './empleados/empleados.component';
 import { ObstetrasComponent } from './obstetras/obstetras.component';
 import { ClinicasComponent } from './clinicas/clinicas.component';
 import { PaisesComponent } from './paises/paises.component';
-import { FormsModule } from '@angular/forms';
 import 'bootstrap';
 import { Olvidecontra2Component } from './olvidecontra2/olvidecontra2.component';
 import { CargaactasComponent } from './cargaactas/cargaactas.component';
@@ -54,9 +54,10 @@ import { AdminComponent } from './admin/admin.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
