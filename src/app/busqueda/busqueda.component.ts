14d3826ee9a1467ce3fd9e3530c2712a3bd7f20a
loudelmar <lourdeslede17@gmail.com>
2021-07-05 20:57:56 -0300

    fixed: busquedaComponent

--- src/app/busqueda/busqueda.component.ts
+++ src/app/busqueda/busqueda.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { MessageService } from 'primeng/api';
-import { ActasService } from './actas-service';
-import { Actas, Representative } from './actas';
 
 @Component({
   selector: 'app-busqueda',
@@ -13,105 +11,43 @@ import { Actas, Representative } from './actas';
 
 export class BusquedaComponent implements OnInit {
 
-  actas: Actas[];
 
-  representatives: Representative[];
-
-  statuses: any[];
-
-  loading: boolean = true;
-
-  activityValues: number[] = [0, 100];
-
-  constructor(private actasService: ActasService) {}
-
-  ngOnInit() {
-    this.actasService.getCustomersLarge().then(actas => {
-      this.actas = actas;
-      this.loading = false;
-
-      this.actas.forEach(
-        actas => (actas.date = new Date(actas.date))
-      );
-    });
-
-    this.representatives = [
-      { name: 'Amy Elsner', image: 'amyelsner.png' },
-      { name: 'Anna Fali', image: 'annafali.png' },
-      { name: 'Asiya Javayant', image: 'asiyajavayant.png' },
-      { name: 'Bernardo Dominic', image: 'bernardodominic.png' },
-      { name: 'Elwin Sharvill', image: 'elwinsharvill.png' },
-      { name: 'Ioni Bowcher', image: 'ionibowcher.png' },
-      { name: 'Ivan Magalhaes', image: 'ivanmagalhaes.png' },
-      { name: 'Onyama Limba', image: 'onyamalimba.png' },
-      { name: 'Stephen Shaw', image: 'stephenshaw.png' },
-      { name: 'XuXue Feng', image: 'xuxuefeng.png' }
-    ];
-
-    this.statuses = [
-      { label: 'Unqualified', value: 'unqualified' },
-      { label: 'Qualified', value: 'qualified' },
-      { label: 'New', value: 'new' },
-      { label: 'Negotiation', value: 'negotiation' },
-      { label: 'Renewal', value: 'renewal' },
-      { label: 'Proposal', value: 'proposal' }
-    ];
-  }
-}
-  
-/*
   filterTerm: string;
 
-  userRecords = [{
-      "name": "Leanne Graham",
-      "username": "Bret",
-      "email": "Sincere@april.biz"
+  Actas = [{
+      "nombre": "Lourdes del Mar Lede",
+      "DNI": "40.775.265",
+      "FechaNacimiento": "17/02/1998",
+      "LugarNacimiento": "La Rioja, Capital",
+      "ActaNacimiento": "VER"
     },
     {
-      "name": "Ervin Howell",
-      "username": "Antonette",
-      "email": "Shanna@melissa.tv"
+      "nombre": "Milva Ernestina Lede",
+      "DNI": "44.123.456",
+      "FechaNacimiento": "24/04/2004",
+      "LugarNacimiento": "La Rioja, Capital",
+      "ActaNacimiento": "VER"
     },
     {
-      "name": "Clementine Bauch",
-      "username": "Samantha",
-      "email": "Nathan@yesenia.net"
+      "nombre": "Guadalupe Esmeralda Lede",
+      "DNI": "01.234.567",
+      "FechaNacimiento": "13/11/2005",
+      "LugarNacimiento": "La Rioja, Capital",
+      "ActaNacimiento": "VER"
     },
     {
-      "name": "Patricia Lebsack",
-      "username": "Karianne",
-      "email": "Julianne.OConner@kory.org"
+      "nombre": "Ángel Gustavo Lede",
+      "DNI": "23.670.990",
+      "FechaNacimiento": "07/01/1974",
+      "LugarNacimiento": "Buenos Aires, Capital Federal",
+      "ActaNacimiento": "VER"
     },
     {
-      "name": "Chelsey Dietrich",
-      "username": "Kamren",
-      "email": "Lucio_Hettinger@annie.ca"
-    },
-    {
-      "name": "Mrs. Dennis Schulist",
-      "username": "Leopoldo_Corkery",
-      "email": "Karley_Dach@jasper.info"
-    },
-    {
-      "name": "Kurtis Weissnat",
-      "username": "Elwyn.Skiles",
-      "email": "Telly.Hoeger@billy.biz"
-    },
-    {
-      "name": "Nicholas Runolfsdottir V",
-      "username": "Maxime_Nienow",
-      "email": "Sherwood@rosamond.me"
-    },
-    {
-      "name": "Glenna Reichert",
-      "username": "Delphine",
-      "email": "Chaim_McDermott@dana.io"
-    },
-    {
-
-      "name": "Clementina DuBuque",
-      "username": "Moriah.Stanton",
-      "email": "Rey.Padberg@karina.biz"
+      "nombre": "Susana Estela Mansilla Kukulis",
+      "DNI": "23.963.589",
+      "FechaNacimiento": "24/06/1974",
+      "LugarNacimiento": "La Rioja, Capital",
+      "ActaNacimiento": "VER"
     }
   ]
 
@@ -123,4 +59,3 @@ export class BusquedaComponent implements OnInit {
   }
 
 }
-*/
