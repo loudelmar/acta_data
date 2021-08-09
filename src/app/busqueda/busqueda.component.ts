d2557c4bdb5402bd3b237ec1af2bc25b645d3e5e
loudelmar <lourdeslede17@gmail.com>
2021-08-08 22:27:33 -0300

    issue: busqueda no funciona

--- src/app/busqueda/busqueda.component.ts
+++ src/app/busqueda/busqueda.component.ts
@@ -9,87 +9,97 @@ import { Component, OnInit } from '@angular/core';
 
 export class BusquedaComponent implements OnInit {
 
-
-  filterTerm: string;
-
+  searchValue: string;
   items = [];
-
+  condition: boolean = null;
+  prevText: string = '';
   actas = [{
-      "nombre": "Lede, Lourdes del Mar",
-      "DNI": "40.775.265",
-      "FechaNacimiento": "17/02/1998",
-      "LugarNacimiento": "La Rioja, Capital",
-      "ActaNacimiento": "VER"
-    },
-    {
-      "nombre": "Lede, Milva Ernestina",
-      "DNI": "44.123.456",
-      "FechaNacimiento": "24/04/2004",
-      "LugarNacimiento": "La Rioja, Capital",
-      "ActaNacimiento": "VER"
-    },
-    {
-      "nombre": "Lede, Guadalupe Esmeralda",
-      "DNI": "01.234.567",
-      "FechaNacimiento": "13/11/2005",
-      "LugarNacimiento": "La Rioja, Capital",
-      "ActaNacimiento": "VER"
-    },
-    {
-      "nombre": "Lede, Ángel Gustavo",
-      "DNI": "23.670.990",
-      "FechaNacimiento": "07/01/1974",
-      "LugarNacimiento": "Buenos Aires, Capital Federal",
-      "ActaNacimiento": "VER"
-    },
-    {
-      "nombre": "Mansilla Kukulis, Susana Estela",
-      "DNI": "23.963.589",
-      "FechaNacimiento": "24/06/1974",
-      "LugarNacimiento": "La Rioja, Capital",
-      "ActaNacimiento": "VER"
-    },
-    {
-      "nombre": "Lede, Lourdes del Mar",
-      "DNI": "40.775.265",
-      "FechaNacimiento": "17/02/1998",
-      "LugarNacimiento": "La Rioja, Capital",
-      "ActaNacimiento": "VER"
-    },
-    {
-      "nombre": "Lede, Milva Ernestina",
-      "DNI": "44.123.456",
-      "FechaNacimiento": "24/04/2004",
-      "LugarNacimiento": "La Rioja, Capital",
-      "ActaNacimiento": "VER"
-    },
-    {
-      "nombre": "Lede, Guadalupe Esmeralda",
-      "DNI": "01.234.567",
-      "FechaNacimiento": "13/11/2005",
-      "LugarNacimiento": "La Rioja, Capital",
-      "ActaNacimiento": "VER"
-    },
-    {
-      "nombre": "Lede, Ángel Gustavo",
-      "DNI": "23.670.990",
-      "FechaNacimiento": "07/01/1974",
-      "LugarNacimiento": "Buenos Aires, Capital Federal",
-      "ActaNacimiento": "VER"
-    },
-    {
-      "nombre": "Mansilla Kukulis, Susana Estela",
-      "DNI": "23.963.589",
-      "FechaNacimiento": "24/06/1974",
-      "LugarNacimiento": "La Rioja, Capital",
-      "ActaNacimiento": "VER"
-    }
+    "nombre": "Lede, Lourdes del Mar",
+    "DNI": "40.775.265",
+    "FechaNacimiento": "17/02/1998",
+    "LugarNacimiento": "La Rioja, Capital",
+    "ActaNacimiento": "VER"
+  },
+  {
+    "nombre": "Lede, Milva Ernestina",
+    "DNI": "44.123.456",
+    "FechaNacimiento": "24/04/2004",
+    "LugarNacimiento": "La Rioja, Capital",
+    "ActaNacimiento": "VER"
+  },
+  {
+    "nombre": "Lede, Guadalupe Esmeralda",
+    "DNI": "01.234.567",
+    "FechaNacimiento": "13/11/2005",
+    "LugarNacimiento": "La Rioja, Capital",
+    "ActaNacimiento": "VER"
+  },
+  {
+    "nombre": "Lede, Ángel Gustavo",
+    "DNI": "23.670.990",
+    "FechaNacimiento": "07/01/1974",
+    "LugarNacimiento": "Buenos Aires, Capital Federal",
+    "ActaNacimiento": "VER"
+  },
+  {
+    "nombre": "Mansilla Kukulis, Susana Estela",
+    "DNI": "23.963.589",
+    "FechaNacimiento": "24/06/1974",
+    "LugarNacimiento": "La Rioja, Capital",
+    "ActaNacimiento": "VER"
+  },
+  {
+    "nombre": "Lede, Lourdes del Mar",
+    "DNI": "40.775.265",
+    "FechaNacimiento": "17/02/1998",
+    "LugarNacimiento": "La Rioja, Capital",
+    "ActaNacimiento": "VER"
+  },
+  {
+    "nombre": "Lede, Milva Ernestina",
+    "DNI": "44.123.456",
+    "FechaNacimiento": "24/04/2004",
+    "LugarNacimiento": "La Rioja, Capital",
+    "ActaNacimiento": "VER"
+  },
+  {
+    "nombre": "Lede, Guadalupe Esmeralda",
+    "DNI": "01.234.567",
+    "FechaNacimiento": "13/11/2005",
+    "LugarNacimiento": "La Rioja, Capital",
+    "ActaNacimiento": "VER"
+  }
+  
+];
+  res_list = [];
+  res_cnt: number = 0;
     
-  ];
+  onSubmit($event){
+    if($event.keyCode === 13){
+      this.condition = true;
+      this.prevText = this.searchValue;
+      this.res_cnt = 0;
+      this.res_list = [];
+      setTimeout(() => {
+        this.condition = false;
+        if(this.actas[i] === this.prevText.toLowerCase()
+             || this.actas[i].startsWith(this.prevText)){
+            this.res_cnt += 1;
+            this.res_list.push(this.actas[i]);
+          
+        }
+      }, 3000);
+      this.searchValue = null;
+    }
+  }
 
-  //paginación
-  pageOfItems: Array<any>;
+    //paginación
+    pageOfItems: Array<any>;
 
+    onChangePage(pageOfItems: Array<any>) {
+      // update current page of items
+      this.pageOfItems = pageOfItems;
+  }
 
   constructor() { }
 
@@ -98,10 +108,4 @@ export class BusquedaComponent implements OnInit {
 
     this.items = Array(150).fill(0).map((x, i) => ({ id: (i + 1)}));
   }
-
-  onChangePage(pageOfItems: Array<any>) {
-    // update current page of items
-    this.pageOfItems = pageOfItems;
-}
-
 }
