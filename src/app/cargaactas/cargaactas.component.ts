2dac4978f3f63ca13788156ecfb9f760a3bfff04
loudelmar <lourdeslede17@gmail.com>
2021-08-15 17:13:59 -0300

    form carga actas

--- src/app/cargaactas/cargaactas.component.ts
+++ src/app/cargaactas/cargaactas.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActasService } from '../_services/actas.service';
 import { Router } from '@angular/router';
+import { Acta } from './acta';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cargaactas',
@@ -9,11 +11,21 @@ import { Router } from '@angular/router';
 })
 export class CargaactasComponent implements OnInit {
 
-  public inscriptos;
+  acta: FormGroup;
+  ngOnInit() {
+    this.acta = new FormGroup({
+      dniInscripto: new FormControl('', [Validators.required, Validators.minLength(8)]),
+      nombreInscripto: new FormControl('', Validators.required),
+      apellidoInscripto: new FormControl('', Validators.required)
+    });
+  }
+}
+  /*public inscriptos;
 
   constructor(private service: ActasService, private router: Router) { }
 
   ngOnInit(): void {
+    
     this.service.getActas().subscribe(dataInscriptos => {
       console.log(dataInscriptos);
       this.inscriptos=dataInscriptos;
@@ -25,3 +37,4 @@ export class CargaactasComponent implements OnInit {
   }
 
 }
+*/
