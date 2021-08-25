67c540b154b474732e3b516177ed7fc950be7e9a
loudelmar <lourdeslede17@gmail.com>
2021-08-25 16:15:49 -0300

    modificaciones admin

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,28 +1,24 @@
 import { Component } from '@angular/core';
-import { first } from 'rxjs/operators';
 
-import { User } from '../_models';
-import { UserService, AuthenticationService } from '../_services';
+import { Role, User } from '../_models';
+import { AuthenticationService } from '../_services';
 
 @Component({ templateUrl: 'inicio.component.html',
 styleUrls: ['./inicio.component.css'] })
 export class InicioComponent {
-    loading = false;
+    title = 'actadata';
     user: User;
-    userFromApi: User;
-
-    constructor(
-        private userService: UserService,
-        private authenticationService: AuthenticationService
-    ) {
-        this.user = this.authenticationService.userValue;
+    
+  
+    constructor(private authenticationService: AuthenticationService){
+      this.authenticationService.user.subscribe(x => this.user = x);
     }
-
-    ngOnInit() {
-        this.loading = true;
-        this.userService.getById(this.user.id).pipe(first()).subscribe(user => {
-            this.loading = false;
-            this.userFromApi = user;
-        });
+  
+    get isAdmin(){
+      return this.user && this.user.role === Role.Admin;
+    }
+  
+    get isUser(){
+      return this.user && this.user.role === Role.User;
     }
 }
