f9ac379e2ca923be847ec8754c1e3a2d4356c351
loudelmar <lourdeslede17@gmail.com>
2021-10-08 00:43:08 -0300

    carga-actas lista

--- src/app/cargaactas/cargaactas.component.ts
+++ src/app/cargaactas/cargaactas.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ActasService } from '../_services/actas.service';
-import { Router } from '@angular/router';
-import { Acta } from './acta';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cargaactas',
@@ -11,30 +7,8 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class CargaactasComponent implements OnInit {
 
-  acta: FormGroup;
-  ngOnInit() {
-    this.acta = new FormGroup({
-      dniInscripto: new FormControl('', [Validators.required, Validators.minLength(8)]),
-      nombreInscripto: new FormControl('', Validators.required),
-      apellidoInscripto: new FormControl('', Validators.required)
-    });
+  constructor(){}
+
+  ngOnInit(): void{
   }
 }
-  /*public inscriptos;
-
-  constructor(private service: ActasService, private router: Router) { }
-
-  ngOnInit(): void {
-    
-    this.service.getActas().subscribe(dataInscriptos => {
-      console.log(dataInscriptos);
-      this.inscriptos=dataInscriptos;
-    });
-  }
-
-  editar(idInscripto:string){
-    this.router.navigate(['/obstetras'])
-  }
-
-}
-*/
